290b972f51568a2e638e80f61bff4c04568f8b72
Goose67986 <nngbusiness@gmail.com>
2025-05-02 20:21:33 -0400

    Add files via upload

--- weather_data.js
+++ weather_data.js
@@ -1,2 +1,2 @@
-const weatherSummary = `Mostly overcast throughout the morning.`;
- Temp will hit 65.1 by noon.const weatherWarning = ``;
+const weatherSummary = `Mostly overcast throughout the morning., Temp will hit 65.1 by noon.`;
+const weatherWarning = ``;
